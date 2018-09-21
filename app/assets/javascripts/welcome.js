48f1c8428b4c234b39979ab071a58f886b067517
Yuriy Ivanov <ymivanov@icloud.com>
2018-09-21 10:57:37 +0200

    logo and responsive navbar

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,2 +1,8 @@
 $(function () {
-});
+    
+    $('.friend-column').on('click', function() {
+        let current = window.location.href.split('/');
+        window.location.href = current[0] + "/" + this.id;
+    });
+    
+}); 
