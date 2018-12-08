128c2bd1a7aebe53d6439035d7d2e70fd7ca986d
Yuriy Ivanov <ymivanov@icloud.com>
2018-12-08 14:44:52 +0100

    fix second navbar collapse transition

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,37 +16,8 @@
 //= require bootstrap
 //= require ckeditor/init
 //= require_tree .
+(function() {
+  console.log("here");
 
-$(function() {
-    console.log('here');
-    var stickyToggle = function(sticky, stickyWrapper, scrollElement) {
-        var stickyHeight = sticky.outerHeight();
-        var stickyTop = stickyWrapper.offset().top;
-        if (scrollElement.scrollTop() >= stickyTop){
-          stickyWrapper.height(stickyHeight);
-          sticky.addClass("is-sticky");
-        }
-        else{
-          sticky.removeClass("is-sticky");
-          stickyWrapper.height('auto');
-        }
-      };
-      
-      // Find all data-toggle="sticky-onscroll" elements
-      $('[data-toggle="sticky-onscroll"]').each(function() {
-        var sticky = $(this);
-        var stickyWrapper = $('<div>').addClass('sticky-wrapper'); // insert hidden element to maintain actual top offset on page
-        sticky.before(stickyWrapper);
-        sticky.addClass('sticky');
-        
-        // Scroll & resize events
-        $(window).on('scroll.sticky-onscroll resize.sticky-onscroll', function() {
-          stickyToggle(sticky, stickyWrapper, $(this));
-        });
-        
-        // On page load
-        stickyToggle(sticky, stickyWrapper, $(window));
-      });
-      
-
-});
+  
+})();
