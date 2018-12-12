4141859e91fd673ff40b39a4b7825781d0226d8b
Yuriy Ivanov <ymivanov@icloud.com>
2018-12-12 23:02:55 +0100

    added locale to app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper ApplicationHelper
 
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    puts I18n.locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,60 @@
 Rails.application.routes.draw do
   
-  get 'about/home'
-
-  get 'about/news'
-
-  get 'about/organization'
-
-  get 'about/history'
+  
 
   devise_for :users, :controllers => { registrations: "registrations", sessions: "sessions"}
 
   mount Ckeditor::Engine => '/ckeditor'
 
-  resources :index_posts
-  resources :workshops
-  resources :galleries
+  scope "(:locale)", locale: /en|uk/ do
+
+
+    #routes for image deletion
+    resources :course_images,  only: [:destroy]
+    resources :index_post_images, only: [:destroy]
+    resources :gallery_images, only: [:create, :destroy]
+    resources :topic_images, only: [:create, :destroy]
+    resources :workshop_images, only: [:create, :destroy]
+
+    resources :courses 
+    resources :galleries
+    resources :workshops
+    #blog post topics
+    resources :topics 
+    resources :index_posts
+
+    resources :teachers, only: [:index, :show]
+
+
+    get 'about/home'
+    get 'about/news'
+    get 'about/organization'
+    get 'about/history'
+
+    get "/admin", to: "admin#index"
+    get "/admin/courses", to: "admin#courses"
+    get "/admin/galleries", to: "admin#galleries"
+    get "/admin/index_pages", to: "admin#index_pages"
+    get "/admin/news_topics", to: "admin#news_topics"
+    get "/admin/workshops", to: "admin#workshops"
+    get "/admin/users", to: "admin#users"
+
+
+    post "admin/confirm_user" => "admin#confirm_user"
+  
+    get 'about' => 'welcome#about'
+    get 'index' => 'welcome#index'
+    get '/' => 'intro#intro'
+
+    root 'welcome#index'
+
+  end
 
   
-  resources :courses 
 
-  #routes for image deletion
-  resources :course_images,  only: [:destroy]
-  resources :index_post_images, only: [:destroy]
-  resources :gallery_images, only: [:create, :destroy]
-  resources :topic_images, only: [:create, :destroy]
-  resources :workshop_images, only: [:create, :destroy]
+  
 
-  resources :teachers, only: [:index, :show]
-
-  #blog post topics
-  resources :topics 
+  
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
@@ -38,20 +63,6 @@ Rails.application.routes.draw do
     post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
 
-  get "/admin", to: "admin#index"
-  get "/admin/courses", to: "admin#courses"
-  get "/admin/galleries", to: "admin#galleries"
-  get "/admin/index_pages", to: "admin#index_pages"
-  get "/admin/news_topics", to: "admin#news_topics"
-  get "/admin/workshops", to: "admin#workshops"
-  get "/admin/users", to: "admin#users"
-
-  post "admin/confirm_user" => "admin#confirm_user"
   
-  get 'about' => 'welcome#about'
-  get 'index' => 'welcome#index'
-  get '/' => 'intro#intro'
-
-  root 'welcome#index'
 
 end
