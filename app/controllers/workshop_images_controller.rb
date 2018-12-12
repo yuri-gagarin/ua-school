d8d6d3ef0076cb0798042f4ef6ee90df8d3d98df
Yuriy Ivanov <ymivanov@icloud.com>
2018-12-12 18:42:01 +0100

    add multiple image functionality to workshops. styling of workshop edit page

--- app/controllers/workshop_images_controller.rb
+++ app/controllers/workshop_images_controller.rb
@@ -1,2 +1,35 @@
 class WorkshopImagesController < ApplicationController
+    include AuthorizationsHelper
+    before_action :authorize_admin
+
+    def create 
+        @workshop_id = params[:workshop_id]
+
+        @workshop_image = WorkshopImage.new(permitted_params)
+        @workshop_image.workshop_id = @workshop_id
+
+        @workshop = Workshop.find(params[:workshop_id])
+        if @workshop_image.save
+            flash[:notice] = "Image succesffuly added"
+            redirect_to edit_workshop_path(@workshop_id)
+        else   
+            flash[:now] = "Image was not saved"   
+        end
+    end
+
+    def destroy 
+        @workshop_image = WorkshopImage.find(params[:id])
+        @workshop_image.destroy
+        respond_to do |format|
+            format.html {redirect_to :back, notice: "Image has been deleted"}
+            format.json {head :no_content}
+            format.js {render layout: false}
+        end
+    end
+
+    private 
+    
+    def permitted_params
+        params.require(:workshop_image).permit(:image)
+    end
 end
