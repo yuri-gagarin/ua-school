96fbea0c39df739dbef2002517eb156157dbe752
Yuriy Ivanov <ymivanov@icloud.com>
2018-12-17 13:56:54 +0100

    fade in animation on gallery when scrolling down

--- app/assets/javascripts/galleries.js
+++ app/assets/javascripts/galleries.js
@@ -1,1 +1,19 @@
-//baguetteBox.run('.compact-gallery', { animation: 'slideIn'});
+$(document).ready(function() {
+    
+    $(".gal-modal-close").on("click", function() {
+        $(".gal-main-modal").hide();
+        $(".modal-backdrop").remove();
+        $("body").removeClass('modal-open');
+    });
+
+
+    $(document).scroll(function() {
+        $(".main-gal-item").each(function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){    
+                $(this).addClass("main-gal-item-show");
+            }
+        })
+    });
+});
