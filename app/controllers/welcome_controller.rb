1676351c8529c97efb4d8bb302424af535cd4d5e
Yuriy Ivanov <ymivanov@icloud.com>
2018-12-19 11:43:50 +0100

    pagination with ajax on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,13 @@
 class WelcomeController < ApplicationController
   def index
     @workshops = Workshop.all
-    @recent_news = Topic.order(created_at: :desc).limit(3);
+    @news_topics = Topic.reorder("created_at DESC").page(params[:page]).per_page(3)
+
+    respond_to do |format|
+      format.html 
+      format.json { render json: @recent_news }
+      format.js
+    end
   end
 
   def about
