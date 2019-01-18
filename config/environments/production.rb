64e5d1f48431d9f2e002b8af3c239d00d5d547aa
Yuriy Ivanov <ymivanov@icloud.com>
2019-01-18 07:20:17 +0200

    sendgrid heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'uaschule.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
     :address => 'smtp.sendgrid.net',
+    :domain => 'uaschule.herokuapp.com',
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
